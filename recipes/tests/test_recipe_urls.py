51409bd56f7d61e10f5485fb1382dcce8e637cf8
Fernanda Martins <>
2025-10-14 11:56:45 -0300

    Testes

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -2,37 +2,21 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-# Create your tests here.
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-    
-    def test_recipe_home_view_returns_status_code_200_ok(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
 
-    def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
 
-    def test_recipe_home_view_loads_no_recipe_fouds_template(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertIn(' <h1>Nenhuma receita encontrada. 😢</h1>', response.content.decode('utf-8'))
-    
-    def test_recipe_categoty_url_is_correct(self):
-        category_url = reverse('recipes:category', kwargs={'category_id':1})
-        self.assertEqual(category_url, '/' \
-        'recipes/category/1')
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
 
-    def test_recipe_categoty_view_return_404_no_recipes_found(self):
-        response = self.client.get(reverse('recipes:category', kwargs={'category_id':1000}))
-        self.assertEqual(response.status_code, 404)
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
 
-    def test_recipe_recipe_url_is_correct(self):
-        recipe_url = reverse('recipes:recipe', args=(1,))
-        self.assertEqual(recipe_url, '/recipes/1')
-
-    def test_recipe_detail_view_return_404_no_recipes_found(self):
-        response = self.client.get(reverse('recipes:recipe', args=(1,)))
-        self.assertEqual(response.status_code, 404)
+# RED - GREEN - REFACTOR
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,20 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
-from recipes import views
+from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),  # Home
-    path(
-        'recipes/category/<int:category_id>',
-        views.category,
-        name="category"
-    ),  # Category
-    path('recipes/<int:id>', views.recipe, name="recipe"),  # Recipe
+    path('', views.home, name="home"),
+    path('recipes/search/', views.search, name="search"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
