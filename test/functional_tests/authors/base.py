92029bca5dbcdde197d25e3e8e257ec18b4af182
Fernanda Martins <>
2025-11-12 12:30:17 -0300

    Ajuste base 2

--- test/functional_tests/authors/base.py
+++ test/functional_tests/authors/base.py
@@ -28,12 +28,14 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
     # 1. Testar se o valor padrão (5 segundos) é usado
     # O patch substitui a função time.sleep do módulo 'time' global
     @patch('time.sleep')
-    def test_sleep_uses_default_five_seconds_if_no_argument_is_given(self, mock_time_sleep):
+    def test_sleep_uses_default_five_seconds_if_no_argument_is_given(
+        self, mock_time_sleep
+    ):
         # Arrange
         base_test = AuthorsBaseTest()
 
         # Act
-        base_test.sleep() # Chama sem argumentos
+        base_test.sleep()  # Chama sem argumentos
 
         # Assert
         # Verifica se time.sleep foi chamado com 5 (o valor padrão)
@@ -54,7 +56,9 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
     # 3. Testar se a função não foi chamada quando não deveria
     # (Exemplo Negativo)
     @patch('time.sleep')
-    def test_sleep_is_not_called_outside_of_method_execution(self, mock_time_sleep):
+    def test_sleep_is_not_called_outside_of_method_execution(
+        self, mock_time_sleep
+    ):
         # Arrange
         # base_test = AuthorsBaseTest()
         # Act/Assert: Nenhuma chamada a base_test.sleep() é feita
